77cf8c2810071751b4f9b981568840d4f8eb3291
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2024-07-17 15:38:12 +0200

    Allow cubit coreform

--- cubitpy/cubit_wrapper/cubit_wrapper_host.py
+++ cubitpy/cubit_wrapper/cubit_wrapper_host.py
@@ -56,7 +56,7 @@ class CubitConnect(object):
         *,
         cubit_args=None,
         cubit_lib=None,
-        interpreter="popen//python=python2.7",
+        interpreter=None,
     ):
         """Initialize the connection between the client (cubit) python interpreter and this one.
         Also load the cubit module in the remote interpreter.
@@ -73,6 +73,9 @@ class CubitConnect(object):
         interpreter: str
             Python interpreter to be used for running cubit.
         """
+
+        if interpreter is None:
+            interpreter = cupy.get_cubit_interpreter()
 
         if cubit_lib is None:
             cubit_lib = cupy.get_cubit_lib_path()
